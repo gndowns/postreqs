e005e5062af19b641e55100f0aee5ca5eac60e92
Gabriel Downs <gndowns@gmail.com>
2017-09-05 00:06:02 -0400

    mvp

--- comp.js
+++ comp.js
@@ -7,29 +7,50 @@ var data = {};
 
 JSDOM.fromURL(URL).then(parseHtml);
 
+function fromCode(code) {
+  if(!data[code]) {
+    data[code] = {
+      code: code,
+      postreqs: []
+    };
+  }
+  return data[code];
+}
+
+function addPostreq(code, postreqCode) {
+  var thisCourse = fromCode(code);
+  thisCourse.postreqs.push(postreqCode);
+}
+
 function parseHtml(dom) {
-  var doc= dom.window.document;
+  var doc = dom.window.document;
   var courses = doc.getElementById('courses');
 
   var children = courses.children;
 
+
   // every 2 children is a complete course
   for(var i=0; i < children.length - 1; i+=2) {
-    var code = children[i].id;
-    data[code] = {code: code};
     var prereqs = children[i+1].children[1].children[3].textContent.split("Prerequisites: ")[1];
-
     prereqs = prereqs.trim().replace('.','').split(", ");
     prereqs = !prereqs[0] ? [] : prereqs
-    data[code].prereqs = prereqs;
+
+    var code = children[i].id;
+    var thisCourse = fromCode(code);
+    thisCourse.prereqs = prereqs;
+
     // look for comp pre reqs
     for(var j=0; j<prereqs.length; j++) {
       comp_pres = prereqs[j].split("COMP ");
-      if(comp_pres.length > 1) {
-        console.log(comp_pres[1].split()[0]);
+
+      for(var k=1; k < comp_pres.length; k++) {
+        var prereqCode = comp_pres[k].split(' ')[0];
+
+        var prereqCourse = fromCode(prereqCode);
+
+        addPostreq(prereqCode, code);
       }
-      console.log();
     }
   }
-  // console.log(JSON.stringify(data,null,2));
+  console.log(JSON.stringify(data,null,2));
 }
